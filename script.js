14c383a4a0f4389d1826facdab3a2f6f5ac19745
Parv Gupta <Parvg555@gmail.com>
2023-03-10 18:54:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function getFormvalue() {
     //Write your code here
+	var x=document.getElementById("form1");
+    let name = ""
+    for (var i=0;i<x.length;i++) {
+        if (x.elements[i].value!='Submit')
+            name += x.elements[i].value + " "
+    }
+
+    alert(name.substring(0,name.length-1));
 
 }
